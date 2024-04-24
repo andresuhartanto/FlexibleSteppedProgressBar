dd71850b41ce004cb6efebb78ae0c813c829dd71
Andre Suhartanto <andresuhartanto28@gmail.com>
2024-04-24 18:04:48 +0800

    Add more width to 1

--- FlexibleSteppedProgressBar/Classes/FSPBTextLayerExtension.swift
+++ FlexibleSteppedProgressBar/Classes/FSPBTextLayerExtension.swift
@@ -26,7 +26,7 @@ extension CATextLayer {
         
         width = CGFloat(CTLineGetTypographicBounds( line, &ascent, &descent, nil))
 
-        width = ceil(width) + 2
+        width = ceil(width) + 1
         
         self.bounds = CGRect(x: 0, y: 0, width: width, height: ceil(ascent+descent))
     }
